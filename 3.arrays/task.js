d98c74b5848d55f1b347c9e75597b86bcb2cc609
NikitaBez <bezmenov.nikita@gmail.com>
2023-02-27 22:40:21 +0900

    created_tasks
    created tasks of homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
-  
+    const isEqual = arr1.length === arr2.length && arr1.every((number, index) => number === arr2[index]);
+    return isEqual;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if(users.some(user => user.gender === gender)){
+    const filterUsers = users.filter(user => user.gender === gender);
+    const getAges = filterUsers.map(user => user.age);
+    const allAges = getAges.reduce((acc, item) => acc + item, 0);
+    const averageAge = allAges / getAges.length
+    return averageAge;
+}else{
+    return 0;
+}
 }
